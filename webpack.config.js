b3d5be66ed87ac019abe9c4db48bae3ca5985186
Vasil Dimitrov <vasil@twentythree.net>
2019-10-06 06:37:55 +0200

    Init project with config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('lib'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: { react: require.resolve("react") }
+  },
+  externals: {
+    react: {
+      root: 'react',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    }
+  }
+};
